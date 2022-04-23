412f16e6aa42c1f63b31c00e37c63230c38d24b5
pierpaolo <pierpaolo@pierpaolo-microhal>
2022-04-23 18:37:37 +0200

    - Code formatting

--- src/main/java/di/uniba/it/wikioie/preprocessing/PreFile.java
+++ src/main/java/di/uniba/it/wikioie/preprocessing/PreFile.java
@@ -8,40 +8,42 @@ import java.io.File;
  * @author angelica
  */
 public class PreFile {
-	
-	private File file;
-	private boolean poison;
-	private int id;
 
-	/**
-	 * Wraps a file with its corresponding id.
-	 * @param file
-	 * @param id
-	 */
-	public PreFile(File file, int id) {
-		this.file = file;
-		poison = false;
-		this.id = id;
-	}
+    private final File file;
+    private final boolean poison;
+    private final int id;
 
-	/**
-	 * Creates a poison file. When a thread receives a poison file, it stops.
-	 */
-	public PreFile() {
-		file = new File("poison");
-		poison = true;
-	}
-	
-	boolean isPoison() {
-		return poison;
-	}
-	
-	File getFile() {
-		return file;
-	}
-	
-	int getId() {
-		return id;
-	}
+    /**
+     * Wraps a file with its corresponding id.
+     *
+     * @param file
+     * @param id
+     */
+    public PreFile(File file, int id) {
+        this.file = file;
+        poison = false;
+        this.id = id;
+    }
+
+    /**
+     * Creates a poison file. When a thread receives a poison file, it stops.
+     */
+    public PreFile() {
+        file = new File("poison");
+        poison = true;
+        this.id = -1;
+    }
+
+    boolean isPoison() {
+        return poison;
+    }
+
+    File getFile() {
+        return file;
+    }
+
+    int getId() {
+        return id;
+    }
 
 }
--- src/main/java/di/uniba/it/wikioie/preprocessing/Preprocess.java
+++ src/main/java/di/uniba/it/wikioie/preprocessing/Preprocess.java
@@ -1,11 +1,10 @@
 package di.uniba.it.wikioie.preprocessing;
 
 /**
-* Preprocess class processes files in order to extract text.
-* 
-* @author angelica
-*/
-
+ * Preprocess class processes files in order to extract text.
+ *
+ * @author angelica
+ */
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,7 +19,6 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.lucene.util.packed.DirectMonotonicReader;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
@@ -28,162 +26,166 @@ import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.ocr.TesseractOCRConfig;
 import org.apache.tika.parser.ocr.TesseractOCRParser;
 import org.apache.tika.parser.pdf.PDFParserConfig;
-import org.glassfish.jersey.model.Parameter;
 import org.xml.sax.SAXException;
 
-public class Preprocess {	
-	
-	private static BlockingQueue<PreFile> in = new ArrayBlockingQueue(10000);
-	private static List<PreprocessThread> list;
-	private int id = 0;
-	private static int inputDocCount = 0;
-	private static int outputDocCount;
-	private static final Logger LOG = Logger.getLogger(Preprocess.class.getName());
-		
-	public Preprocess() { }
+public class Preprocess {
 
-	/**
-	 * Creates a PreFile object for each file and adds it to the queue.
-	 * @param file
-	 * @throws IOException
-	 * @throws TikaException
-	 * @throws SAXException
-	 */
-	private void preprocess(File file) throws IOException, TikaException, SAXException {
-		if(file.isDirectory()) { 	
-			File[] listFiles = file.listFiles();
-			for(File f: listFiles) {
-				preprocess(f);
-			}
-		} else {
-			if(file.isFile()) {
-				inputDocCount++;
-				if(file.length() != 0) {					
-					PreFile prefile = new PreFile(file, id);
-					in.add(prefile);
-					id++;
-				} else {
-					LOG.log(Level.INFO, file.getName() + " is empty");
-				}
-			}				
-		}	
-	}
+    private static final BlockingQueue<PreFile> in = new ArrayBlockingQueue(10000);
+    private static List<PreprocessThread> list;
+    private int id = 0;
+    private static int inputDocCount = 0;
+    private static int outputDocCount;
+    private static final Logger LOG = Logger.getLogger(Preprocess.class.getName());
 
-	/**
-	 * Creates nt PreprocessThread object(s) and adds it to a list
-	 * @param nt number of threads
-	 * @param outputPath
-	 * @param autoParser
-	 * @param metadata
-	 * @param context
-	 * @return list of PreprocessThread object(s)
-	 */
-	private static List<PreprocessThread> initializeThread(int nt, String outputPath, AutoDetectParser autoParser, Metadata metadata, ParseContext context) {
-		List<PreprocessThread> list = new ArrayList<>();
-		for(int i=0; i<nt; i++) {
-			list.add(new PreprocessThread(in, outputPath, autoParser, metadata, context));
-		}
-		return list;
-	}
+    public Preprocess() {
+    }
 
-	/**
-	 * Starts thread(s)
-	 */
-	private void startThread() {
-		for(Thread t: list) {
-			t.start();
-		}
-	}
+    /**
+     * Creates a PreFile object for each file and adds it to the queue.
+     *
+     * @param file
+     * @throws IOException
+     * @throws TikaException
+     * @throws SAXException
+     */
+    private void preprocess(File file) throws IOException, TikaException, SAXException {
+        if (file.isDirectory()) {
+            File[] listFiles = file.listFiles();
+            for (File f : listFiles) {
+                preprocess(f);
+            }
+        } else {
+            if (file.isFile()) {
+                inputDocCount++;
+                if (file.length() != 0) {
+                    PreFile prefile = new PreFile(file, id);
+                    in.add(prefile);
+                    id++;
+                } else {
+                    LOG.log(Level.INFO, "{0} is empty", file.getName());
+                }
+            }
+        }
+    }
 
-	/**
-	 * Closes thread(s)
-	 * @throws InterruptedException
-	 */
-	private void closeThread() throws InterruptedException {
-		poison();
-		for(Thread t: list) {
-			t.join();
-		}
-		LOG.info("Closing...");
-	}
+    /**
+     * Creates nt PreprocessThread object(s) and adds it to a tlist
+     *
+     * @param nt number of threads
+     * @param outputPath
+     * @param autoParser
+     * @param metadata
+     * @param context
+     * @return tlist of PreprocessThread object(s)
+     */
+    private static List<PreprocessThread> initializeThread(int nt, String outputPath, AutoDetectParser autoParser, Metadata metadata, ParseContext context) {
+        List<PreprocessThread> tlist = new ArrayList<>();
+        for (int i = 0; i < nt; i++) {
+            tlist.add(new PreprocessThread(in, outputPath, autoParser, metadata, context));
+        }
+        return tlist;
+    }
 
-	/**
-	 * Creates a poison PreFile for each thread. A poisoned object is added to the queue at last,
-	 * to stop the thread taking it.
-	 */
-	private void poison() {
-		PreFile poison = new PreFile();
-		for(Thread t: list)
-			in.add(poison);
-	}
+    /**
+     * Starts thread(s)
+     */
+    private void startThread() {
+        for (Thread t : list) {
+            t.start();
+        }
+    }
 
-	private void stats() {
-		for(PreprocessThread t: list) {
-			outputDocCount = outputDocCount + t.getDocCount();
-		}
-		LOG.log(Level.INFO, "Input file count: " + inputDocCount);
-		LOG.log(Level.INFO, "Processed file count: " + outputDocCount);
-	}
-	
-	public static void main(String[] args) throws IOException, TikaException, SAXException {
-		System.setProperty("java.util.logging.SimpleFormatter.format",
-				"%4$s: %5$s [%1$tc]%n");
-		Options options = new Options();
-		options = options.addOption(new Option("i", true, "Input directory"))
-				.addOption(new Option("o", true, "Output directory"))
-				.addOption(new Option("t", true, "Number of threads (optional, default 4)"))
-				.addOption(new Option("r", false, "Tesseract OCR (optional, default disabled)" ));
-		try {
-			DefaultParser cmdParser = new DefaultParser();
-			CommandLine cmd = cmdParser.parse(options, args);
-			if(cmd.hasOption("i") && cmd.hasOption("o")) {
-				File inputPath = new File(cmd.getOptionValue("i"));
-				String outputPath = cmd.getOptionValue("o");
-				int nt = Integer.parseInt(cmd.getOptionValue("t", "4"));
-				LOG.log(Level.INFO, "Input dir: {0}", cmd.getOptionValue("i"));
-				LOG.log(Level.INFO, "Output dir: {0}", cmd.getOptionValue("o"));
-				LOG.log(Level.INFO, "Threads: {0}", nt);
+    /**
+     * Closes thread(s)
+     *
+     * @throws InterruptedException
+     */
+    private void closeThread() throws InterruptedException {
+        poison();
+        for (Thread t : list) {
+            t.join();
+        }
+        LOG.info("Closing...");
+    }
 
-				//Initializing parser
-				AutoDetectParser autoParser = new AutoDetectParser();
-				Metadata metadata = new Metadata();
-				PDFParserConfig pdfConfig = new PDFParserConfig();
-				pdfConfig.setExtractInlineImages(true);
-				TesseractOCRConfig ocrConfig = new TesseractOCRConfig();
-				if(cmd.hasOption("r")) {
-					TesseractOCRParser ocrParser = new TesseractOCRParser();
-					if(ocrParser.hasTesseract()) {
-						ocrConfig.setLanguage("ita");
-						LOG.log(Level.INFO, "OCR enabled");
-					} else {
-						ocrConfig.setSkipOcr(true);
-						LOG.log(Level.WARNING, "Tesseract is not installed, can't enable OCR");
-					}
-				} else {
-					ocrConfig = new TesseractOCRConfig();
-					ocrConfig.setSkipOcr(true);
-					LOG.log(Level.INFO, "OCR disabled");
-				}
-				ParseContext context = new ParseContext();
-				context.set(PDFParserConfig.class, pdfConfig);
-				context.set(AutoDetectParser.class, autoParser);
-				context.set(TesseractOCRConfig.class, ocrConfig);
+    /**
+     * Creates a poison PreFile for each thread. A poisoned object is added to
+     * the queue at last, to stop the thread taking it.
+     */
+    private void poison() {
+        PreFile poison = new PreFile();
+        for (Thread t : list) {
+            in.add(poison);
+        }
+    }
 
-				list = initializeThread(nt, outputPath, autoParser, metadata, context);
-				Preprocess pre = new Preprocess();
-				pre.startThread();
-				LOG.info("Starting preprocessing...");
-				pre.preprocess(inputPath);
-				pre.closeThread();
-				pre.stats();
-			}			
-		} catch (ParseException e) {
-			 HelpFormatter formatter = new HelpFormatter();
-	         formatter.printHelp("Preprocess", options);
-		} catch (InterruptedException e) {
-			LOG.log(Level.SEVERE, "An error occurred: ", e);
-		}
-		
-	}
+    private void stats() {
+        for (PreprocessThread t : list) {
+            outputDocCount = outputDocCount + t.getDocCount();
+        }
+        LOG.log(Level.INFO, "Input file count: {0}", inputDocCount);
+        LOG.log(Level.INFO, "Processed file count: {0}", outputDocCount);
+    }
+
+    public static void main(String[] args) throws IOException, TikaException, SAXException {
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%4$s: %5$s [%1$tc]%n");
+        Options options = new Options();
+        options = options.addOption(new Option("i", true, "Input directory"))
+                .addOption(new Option("o", true, "Output directory"))
+                .addOption(new Option("t", true, "Number of threads (optional, default 4)"))
+                .addOption(new Option("r", false, "Tesseract OCR (optional, default disabled)"));
+        try {
+            DefaultParser cmdParser = new DefaultParser();
+            CommandLine cmd = cmdParser.parse(options, args);
+            if (cmd.hasOption("i") && cmd.hasOption("o")) {
+                File inputPath = new File(cmd.getOptionValue("i"));
+                String outputPath = cmd.getOptionValue("o");
+                int nt = Integer.parseInt(cmd.getOptionValue("t", "4"));
+                LOG.log(Level.INFO, "Input dir: {0}", cmd.getOptionValue("i"));
+                LOG.log(Level.INFO, "Output dir: {0}", cmd.getOptionValue("o"));
+                LOG.log(Level.INFO, "Threads: {0}", nt);
+
+                //Initializing parser
+                AutoDetectParser autoParser = new AutoDetectParser();
+                Metadata metadata = new Metadata();
+                PDFParserConfig pdfConfig = new PDFParserConfig();
+                pdfConfig.setExtractInlineImages(true);
+                TesseractOCRConfig ocrConfig = new TesseractOCRConfig();
+                if (cmd.hasOption("r")) {
+                    TesseractOCRParser ocrParser = new TesseractOCRParser();
+                    if (ocrParser.hasTesseract()) {
+                        ocrConfig.setLanguage("ita");
+                        LOG.log(Level.INFO, "OCR enabled");
+                    } else {
+                        ocrConfig.setSkipOcr(true);
+                        LOG.log(Level.WARNING, "Tesseract is not installed, can't enable OCR");
+                    }
+                } else {
+                    ocrConfig = new TesseractOCRConfig();
+                    ocrConfig.setSkipOcr(true);
+                    LOG.log(Level.INFO, "OCR disabled");
+                }
+                ParseContext context = new ParseContext();
+                context.set(PDFParserConfig.class, pdfConfig);
+                context.set(AutoDetectParser.class, autoParser);
+                context.set(TesseractOCRConfig.class, ocrConfig);
+
+                list = initializeThread(nt, outputPath, autoParser, metadata, context);
+                Preprocess pre = new Preprocess();
+                pre.startThread();
+                LOG.info("Starting preprocessing...");
+                pre.preprocess(inputPath);
+                pre.closeThread();
+                pre.stats();
+            }
+        } catch (ParseException e) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("Preprocess", options);
+        } catch (InterruptedException e) {
+            LOG.log(Level.SEVERE, "An error occurred: ", e);
+        }
+
+    }
 
 }
